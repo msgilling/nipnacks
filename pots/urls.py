f5dcc41a8897880bffc4f8d049daaef14a06b719
Mariah Gilling <mxrlling@gmail.com>
2022-01-21 18:03:47 +0000

    get, post, delete and put requests working and tested on insomnia

--- pots/urls.py
+++ pots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PotListView
+from .views import PotDetailView, PotListView
 
 urlpatterns = [
-  path('', PotListView.as_view())
+    path('', PotListView.as_view()),
+    path('<int:pk>/', PotDetailView.as_view())
 ]
--- pots/views.py
+++ pots/views.py
@@ -2,13 +2,57 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Pot
 from .serializers import PotSerializer
 
-# Create your views here.
+# Requests made to /pots/
 class PotListView(APIView):
 
-  def get(self, _request):
-    pots = Pot.objects.all()
-    print('POTS ->', pots)
+    def get(self, _request):
+      pots = Pot.objects.all()
+      print('POTS ->', pots)
+      serialized_pots = PotSerializer(pots, many=True)
+      print('*** SERIALIZED POTS ***', serialized_pots.data)
+      return Response(serialized_pots.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        request.data['owner'] = request.user.id
+        pot = PotSerializer(data = request.data)
+        if pot.is_valid():
+            pot.save()
+            return Response(pot.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(pot.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+# Requests made to /pots/pk/
+class PotDetailView(APIView):
+
+    def get(self, _request, pk):
+        try:
+            pot = Pot.objects.get(id=pk)
+        except:
+            return Response({'message': 'Pot not found'}, status=status.HTTP_404_NOT_FOUND)
+        serialized_pot = PotSerializer(pot)
+        return Response(serialized_pot.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        try:
+            pot = Pot.objects.get(id=pk)
+            pot.delete()
+        except:
+              return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+      try:
+          pot = Pot.objects.get(id=pk)
+      except:
+          return Response({'message': 'Pot not found'}, status=status.HTTP_404_NOT_FOUND)
+      updated_pot = PotSerializer(pot, data=request.data)
+      if updated_pot.is_valid():
+          updated_pot.save()
+          return Response(updated_pot.data, status=status.HTTP_202_ACCEPTED)
+      else:
+          return Response(updated_pot.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
